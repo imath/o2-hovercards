578a0969d0d6d049f88087b8fb6fe9df2dc67d73
imath <math.viet@gmail.com>
2019-08-18 18:59:02 +0200

    Make template and style more generic
    This need to be overriden within the theme or plugin registering the services

--- templates/index.php
+++ templates/index.php
@@ -17,33 +17,16 @@
 			<p class="error">{{data.errorText}}</p>
 
 		<# } else { #>
-			<div class="o2hc-ticket">
-				<# if ( data.meta.Reporter && data.meta.Reporter.avatar_url ) { #>
-					<div class="o2hc-avatar">
-						<img src="{{{data.meta.Reporter.avatar_url}}}" class="avatar">
-					</div>
-				<# } #>
-
-				<div class="o2hc-description" open="true">
-					<div class="o2hc-title">{{data.title}}</div>
-					<div class="o2hc-subtitle">
-						<a href="{{{data.url}}}">{{data.subtitle}}</a>
-					</div>
-
-					<# if ( data.description ) { #>
-						<div class="description">{{{data.description}}}</div>
-					<# } #>
-
-					<# if ( data.keywords ) { #>
-						<ul class="keywords">
-							<# _.each( data.keywords, function( link, tag ) { #>
-								<li><a href="{{link}}">{{tag}}</a></li>
-							<# } ) #>
-						</ul>
-					<# } #>
+			<div class="o2hc-description" open="true">
+				<div class="o2hc-title">{{data.title}}</div>
+				<div class="o2hc-subtitle">
+					<a href="{{{data.url}}}">{{data.subtitle}}</a>
 				</div>
+
+				<# if ( data.description ) { #>
+					<div class="description">{{{data.description}}}</div>
+				<# } #>
 			</div>
 		<# } #>
-
 	</div>
 </script>
